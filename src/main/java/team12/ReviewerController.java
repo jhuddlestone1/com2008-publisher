4547147514443c958ba49fae3597c624297c375d
damilola adeleye <ooadeleye1@sheffield.ac.uk>
2019-11-29 17:35:48 +0000

    a

--- src/main/java/team12/ReviewerController.java
+++ src/main/java/team12/ReviewerController.java
@@ -8,10 +8,24 @@ import java.sql.*;
 
 public class ReviewerController {
 
-    public static void addReview(String title, String summary, String typoList, String verdict, int submissionID, int reviewerID){
-        String query = "INSERT INTO Review(title,summary,typoList,verdict,submissionID,reviewerID) VALUES(?,?,?,?,?)";
-        Object[] vars = {title,summay,typoList,verdict,submissionID,reviewerID};
+    public static void addReview(String title, String summary, String typoList, String iniVerdict, int submissionID, int reviewerID){
+        String query = "INSERT INTO Review(title,summary,typoList,iniVerdict,submissionID,reviewerID) VALUES(?,?,?,?,?,?)";
+        Object[] vars = {title,summary,typoList,iniVerdict,submissionID,reviewerID};
         Query.execute(query,vars);
     }
 
+    public static void addFinalVerdict(String finVerdict, int reviewID){ 
+        String query = "UPDATE Review SET finVerdict = ? WHERE reviewID = ?"; 
+        Object[] vars = {finVerdict,reviewID}; 
+        Query.execute(query, vars); 
+    }
+
+    public static Object[][] getReviews(int submissionID){ 
+        String query = "SELECT * FROM Review WHERE submissionID = ?";
+        Object[] vars = {submissionID}; 
+        Object[][] result = Query.formTable(query,vars);
+        return result;
+
+    }
+    
 } 
