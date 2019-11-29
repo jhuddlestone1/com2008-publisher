148567e2156e6856080fd404b5fa384d99df5927
Eddie Chieng <eskchieng1@sheffield.ac.uk>
2019-11-29 02:01:23 +0000

    very messy Controller.java

--- src/main/java/team12/Controller.java
+++ src/main/java/team12/Controller.java
@@ -1,6 +1,5 @@
 package team12;
 
-import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -9,99 +8,152 @@ import java.util.Date;
 public class Controller {
 
     //All users
-    public static void addUser(String email, int password, String title, String forename, String surname, String uniAffiliation){
+
+    //insert new user information into tables UserLogin and UserDetails
+    public static void addUser(String email, String password, String title, String forename, String surname, String uniAffiliation){
+        //insert into table UserDetails
         String query = "INSERT INTO UserDetails(title,forename,surname,uniAffiliation,email) VALUES(?,?,?,?,?)";
         Object[] vars = {title,forename,surname,uniAffiliation,email};
-        Query.execute(query,vars);
+        Query.execute(query,vars); //execute this first to get the auto incremented userID, to be put into UserLogin
+
+        //insert into table UserLogin
+        //method1 - safer practice
         String queryU = "SELECT userID FROM UserDetails WHERE email=?";
         Object[] varsU = {email};
-        List<List<Object>> table = new ArrayList<>(Query.execute(queryU,varsU));
-        int userID = (Integer) table.get(0).get(0);
+        Object[][] result = Query.formTable(queryU,varsU);
         String query2 = "INSERT INTO UserLogin(email,password,userID) VALUES(?,?,?)";
-        Object[] vars2 = {email, password, userID};
+        Object[] vars2 = {email,password,result[0][0]};
         Query.execute(query2, vars2);
+        // method2 - more efficient but might produce bug when table have same email in different rows (which is not supposed to happen?)
+        // and also practice - VALUES and SELECT shouldn't exist together
+        // String query2 = "INSERT INTO UserLogin(email,password,userID) VALUES(?,?,(SELECT userID FROM UserDetails WHERE email=?))";
+        // Object[] vars2 = {email,password,email};
+        // Query.execute(query2, vars2);
     }
 
-    public static Boolean validateEmail(String email){
-        String query = "SELECT * FROM UserLogin WHERE email=?";
-        Object[] vars = {email};      
-        if (Query.execute(query,vars).isEmpty()) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
+    // check if email exist - applied in validateUser
+    // separate them for different output to user? e.g. "invalid email" || "wrong password"
+    // public static Boolean validateEmail(String email){
+    //     String query = "SELECT * FROM UserLogin WHERE email=?";
+    //     Object[] vars = {email};      
+    //     if (Query.execute(query,vars).isEmpty()) {
+    //         return false;
+    //     }
+    //     else {
+    //         return true;
+    //     }
+    // }
 
-    public static Boolean validateUser(String email, int password){
+    //check if email and password is valid
+    public static Boolean validateUser(String email, String password){
         String query = "SELECT password FROM UserLogin WHERE email=?";
         Object[] vars = {email};
-        List<List<Object>> result = new ArrayList<>(Query.execute(query,vars));      
-        if (!result.isEmpty()){
-            if ((Integer) result.get(0).get(0) == password) {
+        Object[][] result = Query.formTable(query,vars);
+        if (result.length != 0 ){
+            if ( ((String)result[0][0]).equals(password)) {
                 return true;
             }
             else {
+                System.out.println("incorrect password");
                 return false;
             }
         }
         else {
+            System.out.println("invalid email");
             return false;
         }
     }
 
+    //authorise user
+    //input: (email, authEditor, authAuthor, authReviewer)
+    public static void authUser(String email, boolean authEditor, boolean authAuthor, boolean authReviewer){
+        String query = "UPDATE UserDetails SET authEditor=?,authAuthor=?,authReviewer=? WHERE email=?";
+        Object[] vars = {authEditor,authAuthor,authReviewer,email};
+        Query.execute(query,vars);
+    }
+
+    //return user authorisation
+    //output: [authEditor, authAuthor, authReviewer]
+    public static boolean[] userAuth (String email){
+        String query = "SELECT authEditor,authAuthor,authReviewer FROM UserDetails WHERE email=?";
+        Object[] vars = {email};
+        List<List<Object>> table = new ArrayList<>(Query.execute(query,vars));
+        boolean[] result = new boolean[3];
+        for (int x=0; x<3; x++){
+            result[x] = (boolean) table.get(0).get(x);
+        }
+        return result;
+    }
+
     //Journals
+
+    //add journal into table Journal
     public static void addJournal(int ISSN, String journalTitle, int chiefEditorID){
        String query = "INSERT INTO Journal(ISSN, journalTitle, chiefEditorID) VALUES(?,?,?)";
        Object[] vars = {ISSN,journalTitle,chiefEditorID};
        Query.execute(query,vars);
     }
 
+    //return list of journals in a 2d array 
+    //[[ISSN | journalTitle | chiefEditorID]]
     public static Object[][] getJournals(int editorID){
-        String query = "SELECT * FROM Journal j JOIN JournalEditors je on j.ISSN = je.ISSN WHERE editorID=?";
+        String query = "SELECT * FROM Journal WHERE ISSN = (SELECT ISSN FROM JournalEditors WHERE editorID=?)";
         Object[] vars = {editorID};
-        List<List<Object>> table = new ArrayList<>(Query.execute(query,vars));
-        Object[][] journals = new Object[table.size()][table.get(0).size()];
-        for (int x=0; x<table.size(); x++){
-            for (int y=0; y<table.get(0).size(); y++){
-            journals[x][y] = table.get(x).get(y);
-            }
-        }
-        return journals;
+        Object[][] result = Query.formTable(query,vars);
+        return result;
     }
 
+    //add new volume to table Volume
     //date format as string "yyyy-MM-dd" e.g. "2019-11-28"
-    public static void addVolume(String volume, String date, int ISSN) throws ParseException{
+    //try-catch block needed for date formating
+    public static void addVolume(String volume, String date, int ISSN){
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Date date2 = format.parse(date);
-        String query = "INSERT INTO Volume(volume,date,ISSN) VALUES(?,?,?)";
-        Object[] vars = {volume,date2,ISSN};
-        Query.execute(query,vars);
+		try {
+            Date date2 = format.parse(date);    
+            String query = "INSERT INTO Volume(volume,date,ISSN) VALUES(?,?,?)";
+            Object[] vars = {volume,date2,ISSN};
+            Query.execute(query,vars);  
+		} catch (ParseException e)  {
+			e.printStackTrace();
+		}
     }
 
+    //return list of volumes in a 2d array
+    //[[volumeID | ISSN | volume(e.g. Vol 1) | date]]
     public static Object[][] getVolumes(int ISSN){
         String query = "SELECT * FROM Volume WHERE ISSN=?";
         Object[] vars = {ISSN};
-        List<List<Object>> table = new ArrayList<>(Query.execute(query,vars));
-        Object[][] volumes = new Object[table.size()][table.get(0).size()];
-        for (int x=0; x<table.size(); x++){
-            for (int y=0; y<table.get(0).size(); y++){
-            volumes[x][y] = table.get(x).get(y);
-            }
-        }
-        return volumes;
+        Object[][] result = Query.formTable(query,vars);
+        return result;
     }
 
+    //add new edition to table Edition
     //date format as string "yyyy-MM-dd" e.g. "2019-11-28"
-    public static void addEdition(String edition, String date, int volumeID) throws ParseException {
+    public static void addEdition(String edition, String date, int volumeID){
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Date date2 = format.parse(date);
-        String query = "INSERT INTO Edition(edition,date,volumeID) VALUES(?,?,?)";
-        Object[] vars = {edition,date2,volumeID};
-        Query.execute(query,vars);
+		try {
+            Date date2 = format.parse(date);           
+            String query = "INSERT INTO Edition(edition,date,volumeID) VALUES(?,?,?)";
+            Object[] vars = {edition,date2,volumeID};
+            Query.execute(query,vars);  
+		} catch (ParseException e)  {
+			e.printStackTrace();
+		}
+    }
+
+    //return list of volumes in a 2d array
+    //[[editionID | volumeID | edition(e.g. No 1) | date ]]
+    public static Object[][] getEditions(int volumeID){
+        String query = "SELECT * FROM Edition WHERE volumeID=?";
+        Object[] vars = {volumeID};
+        Object[][] result = Query.formTable(query,vars);
+        return result;
     }
 
     //Editors
+
+    //add new editors to table Editors
+    //same problem as addUser - two methods available
     public static void addEditors(int ISSN, String[] emails){
         String query = "INSERT INTO JournalEditors(ISSN,editorID) SELECT ISSN,userID FROM Journal,UserDetails WHERE ISSN=? and email=?";
         for (String email : emails){ 
@@ -110,19 +162,16 @@ public class Controller {
         }
     }
 
+    //return editors in a 2d array
+    //[[userID | title | forename | surname | uniAffiliation | email | authEditor | authAuthor | authReviewer]]
     public static Object[][] getEditors(int ISSN){
-        String query = "SELECT userID FROM JournalEditors WHERE ISSN=?";
+        String query = "SELECT * FROM UserDetails WHERE userID IN (SELECT editorID FROM JournalEditors WHERE ISSN=?)";
         Object[] vars = {ISSN};
-        List<List<Object>> table = new ArrayList<>(Query.execute(query,vars));
-        Object[][] editors = new Object[table.size()][table.get(0).size()];
-        for (int x=0; x<table.size(); x++){
-            for(int y=0; y<table.get(0).size(); y++){
-                editors[x][y] = table.get(x).get(y);
-            }
-        }
-        return editors;
+        Object[][] result = Query.formTable(query,vars);
+        return result;
     }
     
+    //delete editor from table JournalEditors
     public static void deleteEditor(int userID){
         String query = "DELETE FROM JournalEditors WHERE editorID = ?";
         Object[] vars = {userID};
@@ -171,9 +220,9 @@ public class Controller {
     }
 
     public static void main(String[] args){
-        // Object[][] js = getJournals(5);
-        // for (Object[] j : js){
-        //     System.out.println(Arrays.toString(j));
-        // }
+        Object[][] es = getEditions(1);
+        for (Object[] e : es){
+            System.out.println(Arrays.toString(e));
+        }
     }
 }
