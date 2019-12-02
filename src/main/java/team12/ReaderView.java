cec4ef0bae460b6a57ae9c056ca76ac7370233ed
Jamie Huddlestone <aci17jh@sheffield.ac.uk>
2019-12-02 18:58:22 +0000

    Updated ReaderView and SearchPanel

--- src/main/java/team12/ReaderView.java
+++ src/main/java/team12/ReaderView.java
@@ -5,11 +5,16 @@ import javax.swing.*;
 
 public class ReaderView extends AppView {
 	
+	ListPanel resultsPanel = new ListPanel("Results");
+	ListPanel abstractPanel = new ListPanel("Abstract");
+	
 	public ReaderView() {
 		super("wrap 2", "grow", "[][grow]");
-		this.add(new SearchPanel(), "span 2, growx");
-		this.add(new ListPanel("Results"), "grow");
-		this.add(new ListPanel("Abstract"), "grow");
+		this.add(new SearchPanel(), "span 2, growx").addActionListener(
+			new FindArticles(this.searchJournal, this.searchArticle, this.searchText, resultsPanel, abstractPanel)
+		);
+		this.add(resultsPanel, "grow").addActionListener(new AbstractListener(resultsPanel, abstractPanel));
+		this.add(abstractPanel, "grow");
 	}
 	
 }
--- src/main/java/team12/SearchPanel.java
+++ src/main/java/team12/SearchPanel.java
@@ -5,21 +5,21 @@ import javax.swing.*;
 
 public class SearchPanel extends AppPanel {
 	
+	ButtonGroup searchType = new ButtonGroup();
+	JRadioButton searchArticle = new JRadioButton("Article");
+	JRadioButton searchJournal = new JRadioButton("Journal");
+	JTextField searchText = new JTextField(16);
+	JButton searchButton = new JButton("Search");
+	
 	public SearchPanel() {
 		super("", "[][][grow][]");
-		
-		JRadioButton searchArticle = new JRadioButton("Article");
-		JRadioButton searchJournal = new JRadioButton("Journal");
-		ButtonGroup searchType = new ButtonGroup();
 		searchType.add(searchArticle);
 		searchType.add(searchJournal);
 		searchArticle.setSelected(true);
 		this.add(searchArticle);
 		this.add(searchJournal);
-		
-		this.add(new JTextField(16), "grow");
-		this.add(new JButton("Search"));
-		
+		this.add(searchText, "grow");
+		this.add(searchButton);
 		this.setBorder(App.titledBorder("Search"));
 	}
 	
